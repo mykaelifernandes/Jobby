809e16c6b569083c899560d69b2a23348627a730
Mykaeli <mykaelifernandes@hotmail.com>
2023-06-05 14:58:52 -0300

    Update CadastroCandidato.java
    lendo arquivo csv

--- src/CadastroCandidato.java
+++ src/CadastroCandidato.java
@@ -1,5 +1,22 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class CadastroCandidato {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try (Scanner scanner = new Scanner(new File("C:\\Users\\Mykae\\IdeaProjects\\Jobby\\cadastro.csv"))) {
+
+            while (scanner.hasNextLine()) {
+                String linha = scanner.nextLine();
+                String[] campos = linha.split(";");
+
+                String nome = campos[0];
+                System.out.println("Nome: " + nome);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
