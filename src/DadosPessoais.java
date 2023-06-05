f74ef435634af02269d3386b05fbdf6a7cd6a40c
Mykaeli <mykaelifernandes@hotmail.com>
2023-06-05 14:59:38 -0300

    Update DadosPessoais.java
    getters e setters

--- src/DadosPessoais.java
+++ src/DadosPessoais.java
@@ -6,4 +6,39 @@ public class DadosPessoais {
     private String cpf;
     private Date dataNascimento;
     private Enum sexo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Enum getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(Enum sexo) {
+        this.sexo = sexo;
+    }
+
+
+
 }
