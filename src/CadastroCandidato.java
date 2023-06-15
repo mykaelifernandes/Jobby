9dd553d33a0c538892846a5664af738454e0cba8
Mykaeli <mykaelifernandes@hotmail.com>
2023-06-15 17:04:47 -0300

    Leitor CSV

--- src/CadastroCandidato.java
+++ src/CadastroCandidato.java
@@ -1,22 +1,32 @@
-import java.io.File;
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.*;
 
 public class CadastroCandidato {
 
     public static void main(String[] args) {
 
-        try (Scanner scanner = new Scanner(new File("C:\\Users\\Mykae\\IdeaProjects\\Jobby\\cadastro.csv"))) {
+        try (BufferedReader br = new BufferedReader(new FileReader("C:\\Estudos\\Jobby\\cadastro.csv"))) {
 
-            while (scanner.hasNextLine()) {
-                String linha = scanner.nextLine();
-                String[] campos = linha.split(";");
+            String linha;
 
-                String nome = campos[0];
-                System.out.println("Nome: " + nome);
+            while ((linha = br.readLine()) != null) {
+                String[] palavras = linha.split(";");
+
+                    System.out.println("Nome: " + palavras[0]);
+                    System.out.println( "CPF: "  + palavras[1]);
+                    System.out.println( "dataNascimento: " + palavras[2]);
+                    System.out.println( "genero: " + palavras[3]);
+                    System.out.println( "endereco: " + palavras[4]);
+                    System.out.println( "numero: " + palavras[5]);
+                    System.out.println( "complemento: " + palavras[6]);
+                    System.out.println( "bairro: " + palavras[7]);
+                    System.out.println( "cidade: " + palavras[8]);
+                    System.out.println( "estado: " + palavras[9]);
+                    System.out.println( "email: " + palavras[10]);
+
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Erro ao ler o arquivo");
         }
     }
+
 }
